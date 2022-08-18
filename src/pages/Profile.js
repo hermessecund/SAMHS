21725339c8a56766e322dcb985c87a315d760547
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-08-18 02:23:13 +0100

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -17,7 +17,7 @@ const Profile = () => {
     <div className="pfpContainer">
       <img className="profilePFP" src={user.attributes.pfp ? user.attributes.pfp : defaultImgs[0]}></img>
       <div className="profileName">{user.attributes.username.slice(0, 6)}</div>
-      <div className="profileWallet">{`${user.attributes.timezone.slice(0, 4)}...
+      <div className="profileWallet">{`${user.attributes.ethAdress.slice(0, 4)}...
             ${user.attributes.ethAddress.slice(38)}`}</div>
       <Link to="/settings">
           <div className="profileEdit">Edit profile</div>
